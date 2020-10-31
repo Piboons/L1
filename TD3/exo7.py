56c31dbfc28a6c79b71a2446ee2e282dcc9120c5
Piboons <pauline.disalvocilia@gmail.com>
2020-10-31 17:58:09 +0100

    td3

---
+++ TD3/exo7.py
@@ -0,0 +1,11 @@
+﻿import random
+
+nb = random.randint(1,100)
+guess = -1
+while guess != nb:
+	guess = int(input("envoie nombre entre 1 et 100\n"))
+	if guess < nb:
+		print("trop petit")
+	elif guess >= nb:
+		print("trop grand")
+print("trouvé bg")
